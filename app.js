d54084446b39234f9d800bc8b55ebafe78dcc1ce
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-12-01 00:45:05 +0100

    controllers created

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const Thing = require("./models/Thing");
+const stuffRoutes = require("./routes/stuff");
 
 mongoose
   .connect(
@@ -16,42 +16,21 @@ mongoose
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 // utiliser express.json();
 
-app.post("/api/sauces", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...res.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "object saved" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/sauces", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
-});
+//
+app.use("/api/stuff, stuffRoutes");
 
 module.exports = app;
